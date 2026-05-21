108821d48a114ba0a3dc79d056bec1ba1a2aee0c
Filip Bulanda <filipbulanda00@gmail.com>
2026-05-21 11:16:45 +0200

    Download monthly data instead of daily

--- main.r
+++ main.r
@@ -52,64 +52,60 @@ colors = c("blue", "darkgreen", "gold3")
 # Creates dataset. 2023 could not be parsed by hydro_imgw, so it required manual loading. 
 old_enc = getOption("encoding")
 options(encoding = "CP1250")
-data_pre23 = lapply(stations, function(s) {
-  hydro_imgw("daily", year=2015:2022, station=s, allow_failure=FALSE)$COPRZP
-})
-data_post23 = lapply(stations, function(s) {
-  hydro_imgw("daily", year=2024, station=s, allow_failure=FALSE)$COPRZP
-})
+data_full = lapply(stations, function(s) {
+  df = hydro_imgw("monthly", year=2015:2024, station=s, allow_failure=FALSE)
+  df[df$MCWSKEX == 2, ]
+  }
+)
 options(encoding = old_enc)
 
-y23 = read.csv("codz_2023.csv")
-
 data = list(
-  "BOŻEPOLE SZLACHECKIE" = c(data_pre23[[1]], y23[y23$nazwa == "BOŻEPOLE SZLACHECKIE", ]$Q, data_post23[[1]]),
-  "FASTY"                = c(data_pre23[[2]], y23[y23$nazwa == "FASTY", ]$Q,                data_post23[[2]]),
-  "LGOTA NADWARCIE"      = c(data_pre23[[3]], y23[y23$nazwa == "LGOTA NADWARCIE", ]$Q,      data_post23[[3]])
+  "BOŻEPOLE SZLACHECKIE" = data_full[[1]]$MCPRZP,
+  "FASTY"                = data_full[[2]]$MCPRZP,
+  "LGOTA NADWARCIE"      = data_full[[3]]$MCPRZP
 )
 
-max_y = max(data[[1]], data[[2]], data[[3]])
-dates = seq(as.Date("2015-01-01"), as.Date("2024-12-31"), by="day")
+dates = seq(as.Date("2015-01-01"), as.Date("2024-12-31"), by="month")
 
 make_main <- function(id){
   paste("Przepływy na stacji", stations[id], sep=" ")
 }
 
 # Plots
-plot(dates, data[[1]], type="l", ylim=c(0,10), xaxt="n", col=colors[1], main=make_main(1), ylab="Przepływ [m3/s]", xlab="Rok")
+plot(dates, data[[1]], type="l", ylim=c(0,8), xaxt="n", col=colors[1], lwd=2, main=make_main(1), ylab="Przepływ [m3/s]", xlab="Rok")
 axis(1, at=seq(as.Date("2015-01-01"), as.Date("2025-01-01"), by="year"), labels=2015:2025)
 abline(v=seq(as.Date("2015-01-01"), as.Date("2025-01-01"), by="year"), col="black", lty=3)
 abline(h=pretty(data[[1]]), col="black", lty=3)
 
-plot(dates, data[[2]], type="l", ylim=c(0,max_y), xaxt="n", col=colors[2], main=make_main(2), ylab="Przepływ [m3/s]", xlab="Rok")
+plot(dates, data[[2]], type="l", ylim=c(0,30), xaxt="n", col=colors[2], lwd=2, main=make_main(2), ylab="Przepływ [m3/s]", xlab="Rok")
 axis(1, at=seq(as.Date("2015-01-01"), as.Date("2025-01-01"), by="year"), labels=2015:2025)
 abline(v=seq(as.Date("2015-01-01"), as.Date("2025-01-01"), by="year"), col="black", lty=3)
 abline(h=pretty(data[[2]]), col="black", lty=3)
 
-plot(dates, data[[3]], type="l", ylim=c(0,20), xaxt="n", col=colors[3], main=make_main(3), ylab="Przepływ [m3/s]", xlab="Rok")
+plot(dates, data[[3]], type="l", ylim=c(0,5), xaxt="n", col=colors[3], lwd=2, main=make_main(3), ylab="Przepływ [m3/s]", xlab="Rok")
 axis(1, at=seq(as.Date("2015-01-01"), as.Date("2025-01-01"), by="year"), labels=2015:2025)
 abline(v=seq(as.Date("2015-01-01"), as.Date("2025-01-01"), by="year"), col="black", lty=3)
 abline(h=pretty(data[[3]]), col="black", lty=3)
 
-plot(dates, data[[1]], type="l", ylim=c(0,max_y), col=colors[1], xaxt="n", main="Porównanie przepływów", ylab="Przepływ [m3/s]", xlab="Rok")
+plot(dates, data[[1]], type="l", ylim=c(0,30), col=colors[1], xaxt="n", lwd=2, main="Porównanie przepływów", ylab="Przepływ [m3/s]", xlab="Rok")
 axis(1, at=seq(as.Date("2015-01-01"), as.Date("2025-01-01"), by="year"), labels=2015:2025)
 abline(v=seq(as.Date("2015-01-01"), as.Date("2025-01-01"), by="year"), col="black", lty=3)
 abline(h=pretty(data[[2]]), col="black", lty=3)
-lines(dates, data[[2]], col=colors[2])
-lines(dates, data[[3]], col=colors[3])
+lines(dates, data[[2]], col=colors[2], lwd=2)
+lines(dates, data[[3]], col=colors[3], lwd=2)
 legend("topright", legend=stations, col=colors, lty=1)
 
 boxplot(data, names=stations, ylim=c(0, 20), outline=F, col=colors, ylab="Przepływy [m3/s]", main="Porównanie przepływów - boxplot")
 boxplot(data[[1]], data[[3]], names=c(stations[1], stations[3]), ylim=c(0, 5), outline=F, col=c(colors[1], colors[3]), ylab="Przepływy [m3/s]", main="Porównanie przepływów - boxplot")
 
-hist(data[[1]], breaks=seq(0,10,0.5), xlim=c(0,10), ylim=c(0, 1200), las=1, xlab="Przepływ [m3/s]", ylab="Częstość", col=colors[1], main=make_main(1), xaxt="n", labels=T)
-axis(1, at=seq(0,10,0.5))
+hist(data[[1]], breaks=seq(0,7,0.5), xlim=c(0,7), ylim=c(0, 40), las=1, xlab="Przepływ [m3/s]", ylab="Częstość", col=colors[1], main=make_main(1), xaxt="n", labels=T)
+axis(1, at=seq(0,7,0.5))
 
-hist(data[[2]], breaks=seq(0,40,2), xlim=c(0,40), ylim=c(0, 1000), las=1, xlab="Przepływ [m3/s]", ylab="Częstość", col=colors[2], main=make_main(2), xaxt="n", labels=T)
-axis(1, at=seq(0,40,2))
+hist(data[[2]], breaks=seq(0,26,1), xlim=c(0,26), ylim=c(0, 20), las=1, xlab="Przepływ [m3/s]", ylab="Częstość", col=colors[2], main=make_main(2), xaxt="n", labels=T)
+axis(1, at=seq(0,26,1))
 
-hist(data[[3]], breaks=seq(0,20,1), xlim=c(0,20), ylim=c(0, 1600), las=1, xlab="Przepływ [m3/s]", ylab="Częstość", col=colors[3], main=make_main(3), xaxt="n", labels=T)
-axis(1, at=seq(0,20,1))
+hist(data[[3]], breaks=seq(0,5,0.5), xlim=c(0,5), ylim=c(0, 50), las=1, xlab="Przepływ [m3/s]", ylab="Częstość", col=colors[3], main=make_main(3), xaxt="n", labels=T)
+axis(1, at=seq(0,5,0.5))
 
 library(vioplot)
 years_unique = 2015:2024
